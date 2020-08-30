8e269acd535495b9f64f43d449f1dc2b8bdf1df8
antoine staumont <staumont.antoine@me.com>
2020-08-30 17:22:11 +0200

    Add dotenv - Protect my secret data in .env file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,12 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+Rails.application.configure do
+  # [...]
+  # Add this line next to existing config.action_mailer settings
+  config.action_mailer.delivery_method = :letter_opener
+  # [...]
+end
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
