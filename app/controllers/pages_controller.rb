d4360548a4f65d235758bba4d908b5316042521b
antoine staumont <staumont.antoine@me.com>
2020-12-09 14:43:45 +0100

    nouvelle version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,22 @@ class PagesController < ApplicationController
 
   def home
   end
+
+   def chirdents
+  end
+
+   def fabricants
+  end
+
+   def installateurs
+  end
+
+   def innovations
+  end
+
+   def actualites
+  end
+
+   def soutiens
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources "contacts", only: [:new, :create]
+  get 'pages/chirdents'
+  get 'pages/fabricants'
+  get 'pages/installateurs'
+  get 'pages/innovations'
+  get 'pages/actualites'
+  get 'pages/soutiens'
 end
